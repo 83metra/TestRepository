2ab174f2d3ed248dd208b5c3b00e1e68b9cb5cb6
83metra <jumo_004@mail.ru>
2024-11-18 10:34:48 +0200

    Дополнения внесены

--- Test_programm.py
+++ Test_programm.py
@@ -4,6 +4,8 @@ def word_construct(obj):
     print(obj)
     if isinstance(obj, list):
         index=int(input('Введите порядковый номер в списке:'))
+        while index > len(obj) or index < 1:
+            index = int(input('Индекс больше длины списка или отрицательный! Введите ещё раз порядковый номер в списке:'))
         word = obj[index - 1]
         return word
     elif isinstance(obj, (str, int)):
@@ -11,4 +13,7 @@ def word_construct(obj):
 
 my_list = ['Мост', 'Ветвь', 'Синица', 'Дом']
 
+my_int = 45
+
 print(word_construct(my_list))
+print(my_int)
