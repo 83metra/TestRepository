2c9e606308679ca7ef680551d98ee2f8e73d800c
83metra <jumo_004@mail.ru>
2024-11-19 11:02:57 +0200

    Дополнения внесены в Вариант 1.

--- Test_programm.py
+++ Test_programm.py
@@ -1,8 +1,8 @@
-print('Hallo, world')
+print('Hallo, world!')
 
 def word_construct(obj):
     print(obj)
-    if isinstance(obj, list):
+    if isinstance(obj, (list, tuple)):
         index=int(input('Введите порядковый номер в списке:'))
         while index > len(obj) or index < 1:
             index = int(input('Индекс больше длины списка или отрицательный! Введите ещё раз порядковый номер в списке:'))
@@ -10,10 +10,13 @@ def word_construct(obj):
         return word
     elif isinstance(obj, (str, int)):
         return obj
+    else:
+        return obj
 
 my_list = ['Мост', 'Ветвь', 'Синица', 'Дом']
-
+my_str = 'Абра-Кадабра'
 my_int = 45
 
-print(word_construct(my_list))
-print(my_int)
+# print(word_construct(my_list))
+# print(my_int)
+print(my_str)
